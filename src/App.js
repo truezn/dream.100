225fde1252a52de995bf5b871282b3ae6fa700a5
Zhang <ni.zhang@sap.com>
2017-06-09 18:15:07 +0800

    change to use react tutorial code

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to my fist react app by Ning</h2>
+          <h2> Welcome to my fist react app by Ning </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>hello world</h1>
+        <div>
+        <span id="idNavToolBar"> navigation
+        </span>
+        <span id="idContentArea"> content area
+        </span>
+        </div>
       </div>
     );
   }
